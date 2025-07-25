33ac4661cfa0eb21dc874065703b3078ffeaba19
GS-755 <tringuyenminh2003@gmail.com>
2025-07-25 11:14:37 +0700

    20250725 1108 reviewed 23DH114696

--- 23DH114696.sql
+++ 23DH114696.sql
@@ -1,3 +1,5 @@
+-- 20250725 1047 
+-- OK
 CREATE PROC CountByLibrarian
 AS
 BEGIN
@@ -9,6 +11,8 @@ BEGIN
 	ORDER BY acc.nameUser ASC;
 END;
 
+-- 20250725 1047
+-- OK 
 CREATE PROC AddFee @idborrow INT
 AS
 BEGIN
@@ -16,6 +20,8 @@ BEGIN
 	VALUES (0, 0, @idborrow);
 END;
 
+-- 20250725 1048
+-- OK 
 CREATE PROC AddReasonToFee @idfee INT, @idreason INT
 AS
 BEGIN
@@ -23,6 +29,8 @@ BEGIN
 	VALUES (@idfee, @idreason)
 END;
 
+-- 20250725 1049
+-- OK 
 CREATE PROC AddReason @name NVARCHAR(100), @fee FLOAT
 AS
 BEGIN
@@ -30,6 +38,8 @@ BEGIN
 	VALUES (@name, @fee);
 END;
 
+-- 20250725 1050 
+-- OK
 CREATE PROC FindMemberByLate
 AS
 BEGIN
@@ -39,3 +49,45 @@ BEGIN
 	JOIN JOIN_BOOKBORROW j_bb ON j_bb.idBorrow = mt.idBorrow)
 	WHERE j_bb.statusBookBorrow = 'Trễ hạn'
 END;
+-- 20250725 1050
+-- OK 
+CREATE TRIGGER UpdateReqFee
+ON JOIN_LISTREASON
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+	DECLARE @idfee INT, @idreason INT, @fee FLOAT;
+	SET @idfee = ISNULL((SELECT idFee FROM INSERTED), (SELECT idFee FROM DELETED));
+	SET @idreason = ISNULL((SELECT idReason FROM INSERTED), (SELECT idReason FROM DELETED));
+	SET @fee = ISNULL((
+		SELECT tpld.feeReason 
+		FROM INSERTED i
+			JOIN TIENPHATLYDO tpld ON tpld.idReason = i.idReason
+		WHERE tpld.idReason = @idreason), (
+		SELECT tpld.feeReason 
+		FROM DELETED d
+			JOIN TIENPHATLYDO tpld ON tpld.idReason = d.idReason
+		WHERE tpld.idReason = @idreason));
+
+	UPDATE TIENPHAT
+	SET requiredFee = CASE
+		WHEN EXISTS (SELECT 1 FROM INSERTED) THEN requiredFee + @fee
+		WHEN EXISTS (SELECT 1 FROM DELETED) THEN requiredFee - @fee
+	END
+	WHERE idFee = @idFee;
+END;
+
+--Test Data
+EXEC AddUser 0, N'Lê Anh Em', 'asd', '123'
+EXEC AddLibrarian 'asd', 1
+EXEC AddCard '111111111111', N'Đăng Anh Em', 'asd', 'asd', 'asd', '22221222'
+EXEC AddBook '1234', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 0x00
+EXEC AddBorrow 0, '111111111111', 1
+EXEC AddBookToBorrow 1, '1234'
+EXEC UpdateStatus 'JOIN_BOOKBORROW', 1, N'Trễ hạn'
+
+EXEC CountByLibrarian
+EXEC AddFee NULL
+EXEC AddReason 'asd', 100000
+EXEC AddReasonToFee 1, 0
+EXEC FindMemberByLate
