6bda1ccf016ebdd4ed844b1c5432897ea006e163
GS-755 <tringuyenminh2003@gmail.com>
2025-07-31 10:38:32 +0700

    Init commit 21DH114236.sql

---
+++ 21DH114236.sql
@@ -0,0 +1,54 @@
+﻿-- Stored Procedures:
+-- 	Liệt kê top 10 sách được mượn nhiều nhất
+CREATE OR ALTER PROC sp_top10bookborrowed AS BEGIN 
+	SELECT TOP 10 s.* 
+	FROM SACH s INNER JOIN JOIN_BOOKBORROW j 
+		ON(s.idBook = j.idBook);
+END;
+-- Tìm tất cả sách thuộc idTag (dùng JOIN_LISTTAGBOOK và TAG)
+CREATE OR ALTER PROC sp_findbookbytag @idTag INT 
+AS BEGIN 
+	IF NOT EXISTS (
+		SELECT * 
+		FROM Tag
+		WHERE idTag = @idTag
+	) BEGIN
+		Raiserror(N'idTag Không tồn tại!', 16, 1); 
+		RETURN;
+	END;
+	SELECT s.* 
+	FROM SACH s INNER JOIN JOIN_LISTTAGBOOK t 
+		ON(s.idBook = t.idBook)
+	WHERE idTag = @idTag; 
+END; 
+-- Cập nhật trạng thái thành viên dựa vào số sách đang mượn
+CREATE OR ALTER PROC sp_updatememberstatus 
+AS BEGIN 
+	-- Case đang mượn sách 
+	UPDATE DOCGIA 
+		SET StatusMember = N'Bình thường'
+	SELECT mt.idBorrow
+	FROM THETHUVIEN ttv 
+		JOIN MUONTRA mt ON (ttv.idCard = mt.idCard) 
+		JOIN DOCGIA d ON (d.idCard = ttv.idCard)
+		JOIN JOIN_BOOKBORROW jbb ON (mt.idBorrow = jbb.idBorrow)
+	WHERE mt.statusBorrow = N'Đang mượn' OR jbb.statusBookBorrow = N'Đang mượn';
+	-- Case trễ hạn mượn sách 
+	UPDATE DOCGIA 
+		SET StatusMember = N'Trễ hạn mượn sách'
+	SELECT mt.idBorrow
+	FROM THETHUVIEN ttv 
+		JOIN MUONTRA mt ON (ttv.idCard = mt.idCard) 
+		JOIN DOCGIA d ON (d.idCard = ttv.idCard)
+		JOIN JOIN_BOOKBORROW jbb ON (mt.idBorrow = jbb.idBorrow)
+	WHERE mt.statusBorrow = N'Trễ hạn' OR jbb.statusBookBorrow = N'Trễ hạn';
+END;
+-- Gộp thông tin thẻ thư viện, người dùng, và thủ thư trong một phiếu mượn
+CREATE OR ALTER PROC sp_thongtinmuonsach @idCard INT 
+AS BEGIN 
+	SELECT ttv.idCard, ttv.nameCard, d.statusMember, mt.idBorrow, mt.dateBorrow, mt.deadlineDate, mt.statusBorrow, tt.idLibrarian, tt.roleLibrarian
+	FROM THETHUVIEN ttv 
+		JOIN MUONTRA mt ON (ttv.idCard = mt.idCard) 
+		JOIN DOCGIA d ON (d.idCard = ttv.idCard)
+		JOIN THUTHU tt ON (mt.idLibrarian = tt.idLibrarian);
+END;
